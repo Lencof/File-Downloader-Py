3a897165fd9baf97effba25ac7f9c9b8d6861a37
Lencof <71154457+Lencof@users.noreply.github.com>
2021-09-10 12:48:28 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -4,3 +4,8 @@
 from bs4 import BeautifulSoup
 import requests
 from requests import get
+
+
+page = requests.get(input('your URL'))
+filetype = '.' + input('Enter File Extension (with no dot): ')
+soup = BeautifulSoup(page.text, 'html.parser')
