335b2d4460ede57d5737e76d7ccf09dae9651091
Lencof <71154457+Lencof@users.noreply.github.com>
2021-09-10 12:49:18 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -9,3 +9,13 @@ from requests import get
 page = requests.get(input('your URL'))
 filetype = '.' + input('Enter File Extension (with no dot): ')
 soup = BeautifulSoup(page.text, 'html.parser')
+
+for link in soup.find_all('a'):
+    url = link.get('href')
+    if filetype in url:
+        print(url)
+        with open(url, 'wb') as file:
+            response = get(url)
+            file.write(response.content)
+    else:
+        continue
