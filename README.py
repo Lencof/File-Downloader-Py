e2a765db3ab4a4b75d15b7fa9887791a9f8096a3
Lencof <71154457+Lencof@users.noreply.github.com>
2021-09-16 09:44:46 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,24 @@
+# __Author__ __Lencof__
+# README.py
 
+README = '''
+The project was made with the need to upload files to the Could.
+'''
+
+f = open('README.txt', 'w') 
+f.write(README)
+f.close()
+
+# if no mode is specified,
+# 'r'ead mode is assumed by default
+f = open('README.txt') # name file
+while True: 
+    line = f.readline()
+    # Zero length indicates EOF
+    if len(line) == 0:
+        break
+    # The `line` already has a newline
+    # at the end of each line
+    # since it is reading from a file.
+    print(line, end='')
+f.close()
